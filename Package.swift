9fc1d460983add07a5cd667ad6babd3a994bd58b
jtaka1012 <simple.gadget.life@gmail.com>
2025-12-28 17:34:35 +0900

    iOSプラットフォームのバージョンを11から17に更新し、Swiftツールバージョンを5.9に変更しました。また、iOSデプロイメントターゲットを8.0から17.6に更新しました。

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "XLPagerTabStrip",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v17)
     ],
     products: [
         .library(
